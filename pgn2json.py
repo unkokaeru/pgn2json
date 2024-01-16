a83f822d6bb145716c02b7d376e48382d20aea3a
unkokaeru <110934475+unkokaeru@users.noreply.github.com>
2024-01-16 21:13:58 +0000

    Removed PGN files

--- pgn2json.py
+++ pgn2json.py
@@ -8,8 +8,12 @@ import io
 import json
 import shortuuid
 import os
+import typing
 
-def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | None:
+
+def parse_pgn_to_json(
+    pgn: str, POS_FLAGS: dict[str, str]
+) -> dict[str, typing.any] | None:
     """
     Parses a PGN file into a JSON object.
     :param pgn: The PGN file to parse.
@@ -23,13 +27,7 @@ def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | N
 
     # Prepare the header for the JSON object
     title = f"{game.headers['White']} (White, {game.headers['WhiteElo']}) VS {game.headers['Black']} (Black, {game.headers['BlackElo']})"
-    json_data = {
-        "version": "0.0.1",
-        "header": {
-            "title": title
-        },
-        "moves": []
-    }
+    json_data = {"version": "0.0.1", "header": {"title": title}, "moves": []}
 
     # Process each move
     board = game.board()
@@ -39,25 +37,28 @@ def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | N
         before_fen = board.fen()
 
         # Get the flags for the move
-        flags = POS_FLAGS['normal']
+        flags = POS_FLAGS["normal"]
         if board.is_capture(move):
             if board.is_en_passant(move):
-                flags = POS_FLAGS['en_passant']
+                flags = POS_FLAGS["en_passant"]
             else:
-                flags = POS_FLAGS['capture']
+                flags = POS_FLAGS["capture"]
         if board.is_kingside_castling(move):
-            flags = POS_FLAGS['kingside_castling']
+            flags = POS_FLAGS["kingside_castling"]
         if board.is_queenside_castling(move):
-            flags = POS_FLAGS['queenside_castling']
-        if board.piece_type_at(move.from_square) == chess.PAWN and abs(move.from_square - move.to_square) == 16:
-            flags = POS_FLAGS['pawn_push_double']
+            flags = POS_FLAGS["queenside_castling"]
+        if (
+            board.piece_type_at(move.from_square) == chess.PAWN
+            and abs(move.from_square - move.to_square) == 16
+        ):
+            flags = POS_FLAGS["pawn_push_double"]
         if move.promotion:
-            flags += POS_FLAGS['promotion']
+            flags += POS_FLAGS["promotion"]
 
         # Get the piece that was captured
         if board.is_capture(move):
             if board.is_en_passant(move):
-                piece_captured = 'p'
+                piece_captured = "p"
             else:
                 piece_captured = board.piece_at(move.to_square).symbol().lower()
 
@@ -69,7 +70,7 @@ def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | N
         after_fen = board.fen()
 
         # Add the move to the JSON object, depending on the flags
-        if 'c' in flags:
+        if "c" in flags:
             move_info = {
                 "color": "b" if board.turn == chess.WHITE else "w",
                 "piece": piece_moved,
@@ -87,19 +88,18 @@ def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | N
                 "comment": {
                     "type": "doc",
                     "content": [
-                    {
-                        "type": "paragraph",
-                        "content": [
                         {
-                            "type": "text",
-                            "text": extract_comment(game, move)
+                            "type": "paragraph",
+                            "content": [
+                                {"type": "text", "text": extract_comment(game, move)}
+                            ],
                         }
-                        ]
-                    }
-                    ]
-                } if extract_comment(game, move) else None
+                    ],
+                }
+                if extract_comment(game, move)
+                else None,
             }
-        if 'p' in flags:
+        if "p" in flags:
             move_info = {
                 "color": "b" if board.turn == chess.WHITE else "w",
                 "piece": piece_moved,
@@ -117,19 +117,18 @@ def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | N
                 "comment": {
                     "type": "doc",
                     "content": [
-                    {
-                        "type": "paragraph",
-                        "content": [
                         {
-                            "type": "text",
-                            "text": extract_comment(game, move)
+                            "type": "paragraph",
+                            "content": [
+                                {"type": "text", "text": extract_comment(game, move)}
+                            ],
                         }
-                        ]
-                    }
-                    ]
-                } if extract_comment(game, move) else None
+                    ],
+                }
+                if extract_comment(game, move)
+                else None,
             }
-        if 'c' not in flags and 'p' not in flags:
+        if "c" not in flags and "p" not in flags:
             move_info = {
                 "color": "b" if board.turn == chess.WHITE else "w",
                 "piece": piece_moved,
@@ -146,22 +145,22 @@ def parse_pgn_to_json(pgn: str, POS_FLAGS: dict[str, str]) -> dict[str, any] | N
                 "comment": {
                     "type": "doc",
                     "content": [
-                    {
-                        "type": "paragraph",
-                        "content": [
                         {
-                            "type": "text",
-                            "text": extract_comment(game, move)
+                            "type": "paragraph",
+                            "content": [
+                                {"type": "text", "text": extract_comment(game, move)}
+                            ],
                         }
-                        ]
-                    }
-                    ]
-                } if extract_comment(game, move) else None
+                    ],
+                }
+                if extract_comment(game, move)
+                else None,
             }
-        
+
         json_data["moves"].append(move_info)
 
     return json_data
+
 
 def extract_comment(node, move: chess.Move) -> str | None:
     """
@@ -177,9 +176,12 @@ def extract_comment(node, move: chess.Move) -> str | None:
         # If the move is found, return the comment
         if next_node.move == move:
             # Replace newlines with spaces
-            return str(next_node.comment).replace("\n"," ") if next_node.comment else None
+            return (
+                str(next_node.comment).replace("\n", " ") if next_node.comment else None
+            )
         node = next_node
     return None
+
 
 def main() -> None:
     """
@@ -191,17 +193,21 @@ def main() -> None:
     SAV_LOC = "C:\\Users\\wills\\Documents\\GitHub\\digital-garden\\content\\.obsidian\\plugins\\chess-study\\storage\\"
     PGN_LOC = "C:\\Users\\wills\\Downloads\\"
     POS_FLAGS: dict[str, str] = {
-            'normal' : 'n',
-            'capture' : 'c',
-            'en_passant' : 'e',
-            'kingside_castling' : 'k',
-            'queenside_castling' : 'q',
-            'pawn_push_double' : 'b',
-            'promotion' : 'p',
-        }
+        "normal": "n",
+        "capture": "c",
+        "en_passant": "e",
+        "kingside_castling": "k",
+        "queenside_castling": "q",
+        "pawn_push_double": "b",
+        "promotion": "p",
+    }
 
     # Load a list of PGN files (".pgn") from the PGN_LOC directory
-    pgn_files: list[str] = [f for f in os.listdir(PGN_LOC) if os.path.isfile(os.path.join(PGN_LOC, f)) and f.endswith(".pgn")]
+    pgn_files: list[str] = [
+        f
+        for f in os.listdir(PGN_LOC)
+        if os.path.isfile(os.path.join(PGN_LOC, f)) and f.endswith(".pgn")
+    ]
 
     # Print the PGN files found
     print("PGN files found:")
@@ -212,18 +218,19 @@ def main() -> None:
     print("Corresponding chessStudyIds: ")
     for pgn_file in pgn_files:
         # Load the PGN file and convert it to JSON
-        with open(PGN_LOC+pgn_file, "r") as f:
+        with open(PGN_LOC + pgn_file, "r") as f:
             json_output = parse_pgn_to_json(f.read(), POS_FLAGS)
 
         # Generate a random file name
         file_name = shortuuid.ShortUUID().random(length=21)
 
         # Save the JSON file
-        with open(SAV_LOC+file_name+".json", "w") as f:
+        with open(SAV_LOC + file_name + ".json", "w") as f:
             json.dump(json_output, f, indent=2)
 
         # Print the chessStudyId
         print(f"```chessStudy\n chessStudyId: {file_name}\n```\n")
+
 
 if __name__ == "__main__":
     main()
